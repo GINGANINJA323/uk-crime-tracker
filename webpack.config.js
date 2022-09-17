0ddc7e8bf7bbea55b44bfa3e2a11917844a0a574
Edward Glendinning <edward82009@hotmail.com>
2022-09-17 19:56:38 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-typescript"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: [".ts", ".js", ".tsx"] },
+  output: {
+    path: path.resolve(__dirname, "public/"),
+    publicPath: "/public/",
+    filename: "bundle.js"
+  }
+};
