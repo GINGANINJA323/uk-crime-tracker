10a62ec2c24e8c82871c84a6538e9cc39da7f4b6
Edward Glendinning <edward82009@hotmail.com>
2022-09-18 14:21:57 +0100

    Configure webpack

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App = (): JSX.Element => {
-  return (<></>);
+  return (
+    <>
+      <h1>{'UK Police Crime Tracker'}</h1>
+      <p>{'This tool can be used to see recent and historic crimes near to you, from the UK\'s Crime API.'}</p>
+      <h2>{'Crimes in your area:'}</h2>
+    </>
+  );
 }
 
 export default App;
